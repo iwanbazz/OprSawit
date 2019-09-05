e1868159ffe3ab3900e122f92e8198770192eb86
iwanbazz <iwan.bazz@gmail.com>
2019-09-05 18:16:21 +0700

    Create widget mobil

---
+++ app/Widgets/Cars.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Car;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use Arrilot\Widgets\AbstractWidget;
+
+class Cars extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Car::count();
+        $string = 'Mobil';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-news',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => __('voyager::dimmer.post_link_text'),
+                'link' => route('voyager.cars.index'),
+            ],
+            'image' => '/mobil-bg.jpg',
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return app('VoyagerAuth')->user()->can('browse', app(Car::class));
+    }
+}
