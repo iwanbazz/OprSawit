aeda2ecc29f1b5cedb2f7d9ba0a3e33a1dfe323c
iwanbazz <iwan.bazz@gmail.com>
2019-09-10 09:15:12 +0700

    Change widget button color

--- resources/views/vendor/voyager/dimmer.blade.php
+++ resources/views/vendor/voyager/dimmer.blade.php
@@ -4,6 +4,6 @@
         @if (isset($icon))<i class='{{ $icon }}'></i>@endif
         <h4>{!! $title !!}</h4>
         <p>{!! $text !!}</p>
-        <a href="{{ $button['link'] }}" class="btn btn-primary">{!! $button['text'] !!}</a>
+        <a href="{{ $button['link'] }}" class="btn btn-primary" style="background-color:#239650">{!! $button['text'] !!}</a>
     </div>
 </div>
