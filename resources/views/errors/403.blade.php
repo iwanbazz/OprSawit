62af450b256259c662bc6025af5dcd2a654338ce
iwanbazz <iwan.bazz@gmail.com>
2019-09-10 13:41:23 +0700

    Change to indonesian

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __($exception->getMessage() ?: 'Anda tidak diijinkan untuk mengakses halaman ini'))
--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Page Expired'))
 @section('code', '419')
-@section('message', __('Page Expired'))
+@section('message', __('Halaman yang yang anda akses sudah melewati batas waktu yang ditentukan'))
