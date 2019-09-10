0acbc482c9b29a492b76116a9a3b1d3c30aaa073
iwanbazz <iwan.bazz@gmail.com>
2019-09-10 08:43:26 +0700

    Added surat jalan

---
+++ app/Opr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Opr extends Model
+{
+    
+}
---
+++ database/migrations/2019_09_10_012750_created_oprs_table_with_voyager0910e07287e8.php
@@ -0,0 +1,388 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use TCG\Voyager\Database\DatabaseUpdater;
+use TCG\Voyager\Database\Schema\SchemaManager;
+use TCG\Voyager\Database\Types\Type;
+
+class CreatedOprsTableWithVoyager0910e07287e8 extends Migration
+{
+    public function __construct()
+    {
+        Type::registerCustomPlatformTypes();
+    }
+    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Generated only to work with Voyager
+        // upHash=077dd0cdfb453f73c4933a25fdb9cd0d
+        SchemaManager::createTable(
+            [
+              "name" => "oprs",
+              "oldName" => "oprs",
+              "columns" => [
+                "0" => [
+                  "name" => "id",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "1",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "1",
+                  "autoincrement" => "1",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "id",
+                  "null" => "NO",
+                  "extra" => "auto_increment",
+                  "composite" => "",
+                  ],
+                "1" => [
+                  "name" => "created_at",
+                  "type" => [
+                    "name" => "timestamp",
+                    "category" => "Date and Time",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "created_at",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "2" => [
+                  "name" => "updated_at",
+                  "type" => [
+                    "name" => "timestamp",
+                    "category" => "Date and Time",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "updated_at",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "3" => [
+                  "name" => "deleted_at",
+                  "type" => [
+                    "name" => "timestamp",
+                    "category" => "Date and Time",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "deleted_at",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "4" => [
+                  "name" => "contract_id",
+                  "type" => [
+                    "name" => "varchar",
+                    "category" => "Strings",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "contract_id",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "5" => [
+                  "name" => "po_number",
+                  "type" => [
+                    "name" => "varchar",
+                    "category" => "Strings",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "po_number",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "6" => [
+                  "name" => "load_date",
+                  "type" => [
+                    "name" => "datetime",
+                    "category" => "Date and Time",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "load_date",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "7" => [
+                  "name" => "unload_date",
+                  "type" => [
+                    "name" => "datetime",
+                    "category" => "Date and Time",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "unload_date",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "8" => [
+                  "name" => "cost",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "cost",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "9" => [
+                  "name" => "load_weight",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "load_weight",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "10" => [
+                  "name" => "unload_weight",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "unload_weight",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "11" => [
+                  "name" => "decrease",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "decrease",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "12" => [
+                  "name" => "decrease_claim",
+                  "type" => [
+                    "name" => "integer",
+                    "category" => "Numbers",
+                    "default" => [
+                      "type" => "number",
+                      "step" => "any",
+                      ],
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "decrease_claim",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                "13" => [
+                  "name" => "basis",
+                  "type" => [
+                    "name" => "varchar",
+                    "category" => "Strings",
+                    ],
+                  "default" => null,
+                  "notnull" => "",
+                  "length" => null,
+                  "precision" => "10",
+                  "scale" => "0",
+                  "fixed" => "",
+                  "unsigned" => "",
+                  "autoincrement" => "",
+                  "columnDefinition" => null,
+                  "comment" => null,
+                  "oldName" => "basis",
+                  "null" => "YES",
+                  "extra" => "",
+                  "composite" => "",
+                  ],
+                ],
+              "indexes" => [
+                "0" => [
+                  "name" => "primary",
+                  "oldName" => "primary",
+                  "columns" => [
+                    "0" => "id",
+                    ],
+                  "type" => "PRIMARY",
+                  "isPrimary" => "1",
+                  "isUnique" => "1",
+                  "isComposite" => "",
+                  "flags" => [
+                    ],
+                  "options" => [
+                    ],
+                  "table" => "oprs",
+                  ],
+                ],
+              "primaryKeyName" => "primary",
+              "foreignKeys" => [
+                ],
+              "options" => [
+                "collate" => "utf8_unicode_ci",
+                "charset" => "utf8",
+                ],
+              ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
+
+        
