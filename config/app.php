0374009e687adfd313309f654a7c7292759492c9
iwanbazz <iwan.bazz@gmail.com>
2019-09-20 02:25:56 +0700

    Added sentry

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         Larapack\Hooks\HooksServiceProvider::class,
         Larapack\VoyagerHooks\VoyagerHooksServiceProvider::class,
+        Sentry\Laravel\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -221,6 +222,7 @@ return [
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Sentry' => Sentry\Laravel\Facade::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'Str' => Illuminate\Support\Str::class,
