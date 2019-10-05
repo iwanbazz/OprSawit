2c252ba36ad08ad5b57e84e0ae9064a7c36b79fa
iwanbazz <iwan.bazz@gmail.com>
2019-10-05 10:07:28 +0700

    Try to change carbon to 24h format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,25 +7,26 @@ use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        config(['app.locale' => 'id']);
-        Carbon::setLocale('id');
-        date_default_timezone_set('Asia/Jakarta');
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    config(['app.locale' => 'id']);
+    Carbon::setUTF8(true);
+    Carbon::setLocale('id_ID');
+    date_default_timezone_set('Asia/Jakarta');
+  }
 }
